662cea9cb5ac9b251be94d5a51a9f05244ccaa48
Victor Hernandez-Urbina <hernandezurbina@gmail.com>
2019-03-28 17:11:01 +0000

    Added basic MLKit functionality to detect text

--- TextRecognitionMLKit/ViewController.swift
+++ TextRecognitionMLKit/ViewController.swift
@@ -13,22 +13,33 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     let imagePicker = UIImagePickerController()
     
+    var textRecognizer: VisionTextRecognizer!
+    
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var findTextButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        findTextButton.isEnabled = false
         
         imagePicker.delegate = self
         imagePicker.allowsEditing = false
         //imagePicker.sourceType = .camera
         imagePicker.sourceType = .photoLibrary
         
+        let vision = Vision.vision()
+        textRecognizer = vision.onDeviceTextRecognizer()
+        
     }
 
+    // MARK: Camera button functionality
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
     }
+    
+    // MARK: Image picking
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
@@ -36,7 +47,56 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         imageView.image = userPickedImage as? UIImage
         
-        imagePicker.dismiss(animated: true, completion: nil)
+        imagePicker.dismiss(animated: true, completion: {
+            self.findTextButton.isEnabled = true
+        })
+    }
+    
+    // MARK: Find Text button functionality
+    @IBAction func findTextDidTouch(_ sender: UIBarButtonItem) {
+        runTextRecognition(with: imageView.image!)
+    }
+    
+    // MARK: Text Recognition
+    func runTextRecognition(with image: UIImage){
+        let visionImage = VisionImage(image: image)
+        textRecognizer.process(visionImage) { (features, error) in
+            self.processResult(from: features, error: error)
+        }
+    }
+    
+    // MARK: Image Drawing:
+    func processResult(from text: VisionText?, error: Error?){
+        //removeFrames()
+        guard let features = text, let image = imageView.image else {return}
+        
+        for block in features.blocks{
+            
+//            let blockText = block.text
+//            print(blockText)
+//            print()
+            
+            for line in block.lines{
+
+//                let lineText = line.text
+//                print(lineText)
+//                print()
+                
+                for element in line.elements{
+                    //self.addFrameView(featureFrame: element.frame, imageSize: image.size, viewFrame: self.imageView.frame, text: element.text)
+                    
+                    let elementText = element.text
+                    print(elementText)
+                    print()
+                    
+                }
+            }
+        }
+        
+    }
+    
+    func addFrameView(featureFrame: CGRect, imageSize: CGSize, viewFrame: CGRect, text: String? = nil){
+        print("Frame: \(featureFrame)")
     }
     
 }
