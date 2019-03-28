cbd984a186f2e902e447dc69dbc4e2f199f90309
Victor Hernandez-Urbina <hernandezurbina@gmail.com>
2019-03-28 16:08:47 +0000

    First commit: added functionality to capture photo from library/camera, plus Firebase conf

--- TextRecognitionMLKit/ViewController.swift
+++ TextRecognitionMLKit/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import FirebaseMLVision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        //imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
+        
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
+        
+        imageView.image = userPickedImage as? UIImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
