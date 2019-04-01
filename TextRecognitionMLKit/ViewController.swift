9c2c07c775f5ced1b6a7935f62598b17a69291ba
Victor Hernandez-Urbina <hernandezurbina@gmail.com>
2019-04-01 17:20:59 +0100

    Added functionality to show rectangles in detected text, plus functionality to remove them from image.

--- TextRecognitionMLKit/ViewController.swift
+++ TextRecognitionMLKit/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         let vision = Vision.vision()
         textRecognizer = vision.onDeviceTextRecognizer()
-        
+                
     }
 
     // MARK: Camera button functionality
@@ -62,7 +62,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let visionImage = VisionImage(image: image)
         textRecognizer.process(visionImage) { (features, error) in
             self.processResult(from: features, error: error)
+            
         }
+    }
+    
+    @IBAction func trashButtonPressed(_ sender: UIBarButtonItem) {
+        removeFrames()
     }
     
     // MARK: Image Drawing:
@@ -71,21 +76,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         guard let features = text, let image = imageView.image else {return}
         
         for block in features.blocks{
-            
-//            let blockText = block.text
-//            print(blockText)
-//            print()
-            
             for line in block.lines{
-
-//                let lineText = line.text
-//                print(lineText)
-//                print()
-                
                 for element in line.elements{
-                    //self.addFrameView(featureFrame: element.frame, imageSize: image.size, viewFrame: self.imageView.frame, text: element.text)
-                    
                     let elementText = element.text
+                    let elementFrame = element.frame
+                    let scaledElementFrame = createScaledFrame(featureFrame: elementFrame, imageSize: image.size, viewFrame: imageView.frame)
+
+                    let layer = CAShapeLayer()
+                    layer.path = UIBezierPath(rect: scaledElementFrame).cgPath
+                    layer.strokeColor = Constants.lineColor
+                    layer.fillColor = Constants.fillColor
+                    layer.lineWidth = Constants.lineWidth
+                    view.layer.addSublayer(layer)
+//                    self.frameSublayer.addSublayer(layer)
+                    
                     print(elementText)
                     print()
                     
@@ -93,10 +97,64 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
         }
         
+
     }
     
-    func addFrameView(featureFrame: CGRect, imageSize: CGSize, viewFrame: CGRect, text: String? = nil){
-        print("Frame: \(featureFrame)")
+    // MARK: Highlighting Text Frames:
+    private enum Constants{
+        static let lineWidth: CGFloat = 3.0
+        static let lineColor = UIColor.yellow.cgColor
+        static let fillColor = UIColor.clear.cgColor
+    }
+
+    // 1
+    private func createScaledFrame(
+        featureFrame: CGRect,
+        imageSize: CGSize, viewFrame: CGRect)
+        -> CGRect {
+            let viewSize = viewFrame.size
+            
+            // 2
+            let resolutionView = viewSize.width / viewSize.height
+            let resolutionImage = imageSize.width / imageSize.height
+            
+            // 3
+            var scale: CGFloat
+            if resolutionView > resolutionImage {
+                scale = viewSize.height / imageSize.height
+            } else {
+                scale = viewSize.width / imageSize.width
+            }
+            
+            // 4
+            let featureWidthScaled = featureFrame.size.width * scale
+            let featureHeightScaled = featureFrame.size.height * scale
+            
+            // 5
+            let imageWidthScaled = imageSize.width * scale
+            let imageHeightScaled = imageSize.height * scale
+            let imagePointXScaled = (viewSize.width - imageWidthScaled) / 2
+            let imagePointYScaled = (viewSize.height - imageHeightScaled) / 2
+            
+            // 6
+            let featurePointXScaled = imagePointXScaled + featureFrame.origin.x * scale
+            let featurePointYScaled = 60 - imagePointYScaled + featureFrame.origin.y * scale
+            
+            // 7
+            return CGRect(x: featurePointXScaled,
+                          y: featurePointYScaled,
+                          width: featureWidthScaled,
+                          height: featureHeightScaled)
+    }
+
+    private func removeFrames() {
+        guard let sublayers = view.layer.sublayers else { return }
+        
+        for sublayer in sublayers {
+            if type(of: sublayer) != CALayer.self {
+                sublayer.removeFromSuperlayer()
+            }
+        }
     }
     
 }
